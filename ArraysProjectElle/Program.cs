e67d376a2020e052e4ad94866698999729baa6a1
Emmanuelle-Kirk <wolfeetried@gmail.com>
2021-10-12 10:28:52 -0300

    ammo ints

--- ArraysProjectElle/Program.cs
+++ ArraysProjectElle/Program.cs
@@ -8,10 +8,21 @@ namespace ArraysProjectElle
 {
     class Program
     {
+        static int health = 100;
+        static int weapon;
+
+        static void Weapon()
+        {
+            int[] ammo;
+            ammo = new int[3]; //initialization (telling the compiler the size)
+            ammo[0] = 6; //pistol
+            ammo[1] = 2; //shotgun
+            ammo[2] = 10; //bfg
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Weapon System");
-
+            Weapon();
             Console.ReadKey(true);
         }
     }
